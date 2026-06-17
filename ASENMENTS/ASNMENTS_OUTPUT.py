1a3f8fe9448a95f3cdc11a74067b1d7d5db128d5
Santhosha K B <santhuvirat03@gmail.com>
2026-06-17 09:23:13 +0530

    Create ASNMENTS_OUTPUT.py

---
+++ ASENMENTS/ASNMENTS_OUTPUT.py
@@ -0,0 +1,12 @@
+
+
+
+_____________out put.___________
+enter youre name:SANTHOSHA.K.B
+enter your age:17
+enter your color:BLUE
+SANTHOSHA.K.B
+your age: 17 year old
+youre favroti color: BLUE
+
+=== Code Execution Successful ===
