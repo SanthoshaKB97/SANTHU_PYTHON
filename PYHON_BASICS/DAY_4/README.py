e8a3045e4038ecf3ab625f07cb318b6f7e3c2765
Santhosha K B <santhuvirat03@gmail.com>
2026-06-19 16:34:40 +0530

    Update README.py

--- PYHON_BASICS/DAY_4/README.py
+++ PYHON_BASICS/DAY_4/README.py
@@ -1,1 +1,38 @@
 
+Original List:
+['Apple', 'Banana', 'Mango', 'Orange']
+
+First Fruit: Apple
+Last Fruit: Orange
+
+First 2 Fruits: ['Apple', 'Banana']
+Last 2 Fruits: ['Mango', 'Orange']
+
+After Append:
+['Apple', 'Banana', 'Mango', 'Orange', 'Grapes']
+
+After Insert:
+['Apple', 'Pineapple', 'Banana', 'Mango', 'Orange', 'Grapes']
+
+After Remove:
+['Apple', 'Pineapple', 'Mango', 'Orange', 'Grapes']
+
+Removed Item: Grapes
+['Apple', 'Pineapple', 'Mango', 'Orange']
+
+Total Fruits: 4
+Mango is available
+
+Sorted List:
+['Apple', 'Mango', 'Orange', 'Pineapple']
+
+Reverse List:
+['Pineapple', 'Orange', 'Mango', 'Apple']
+
+Using For Loop:
+Pineapple
+Orange
+Mango
+Apple
+
+How many numbers do you want to add? 
