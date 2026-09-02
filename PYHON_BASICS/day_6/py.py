1c9ce56bd0fd7ced6cac0d0f32d1865908aef2a4
Santhosha K B <santhuvirat03@gmail.com>
2026-09-02 20:32:50 +0530

    Update py.py

--- PYHON_BASICS/day_6/py.py
+++ PYHON_BASICS/day_6/py.py
@@ -11,3 +11,12 @@ print(student)
 print(student["name"])
 print(student["age"])
 print(student["marks"])
+
+student = {
+    "name": "Santhu",
+    "age": 17,
+    "marks": 85
+}
+
+for key in student:
+    print(key)
