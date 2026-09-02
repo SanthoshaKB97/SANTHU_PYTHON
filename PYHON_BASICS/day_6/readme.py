51773cdae25b6f0b17d8fac9275680bd6ffa5077
Santhosha K B <santhuvirat03@gmail.com>
2026-09-02 20:35:51 +0530

    Update and rename readme.java to readme.py

---
+++ PYHON_BASICS/day_6/readme.py
@@ -0,0 +1,15 @@
+{'name': 'Santhu', 'age': 17, 'marks': 85}
+Santhu
+17
+85
+name
+age
+marks
+Student Information
+-------------------
+Name: Santhu
+Age: 17
+Course: CSE
+Marks: 85
+
+=== Code Execution Successful ===
