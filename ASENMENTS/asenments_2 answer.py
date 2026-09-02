0f0c1af25dd7e08cb04d3b22c40359b7ab8b11bd
Santhosha K B <santhuvirat03@gmail.com>
2026-09-02 20:39:44 +0530

    Create asenments_2 answer.py

---
+++ ASENMENTS/asenments_2 answer.py
@@ -0,0 +1,19 @@
+Name: Santhu
+Age: 17
+Course: CSE
+Marks: 85
+Updated Marks: 95
+City: Bangalore
+
+Mobile Information
+Brand: Samsung
+Model: S25
+Price: 80000
+Storage: 256GB
+
+Subject Marks
+Maths : 85
+Python : 90
+Java : 80
+
+=== Code Execution Successful ===
